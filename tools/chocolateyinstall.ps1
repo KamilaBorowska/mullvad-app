5046c3acc4b3470932e6a3e6a5a62221a7972a75
Konrad Borowski <konrad@borowski.pw>
2019-06-18 00:17:27 +0200

    Update Mullvad to 2019.5

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,16 +4,16 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $trustedPublisherCertificate = "$toolsDir\openvpn_trusted_publisher.cer"
 $trustedPublisherCertificateHash = '8f53adb36f1c61c50e11b8bdbef8d0ffb9b26665a69d81246551a0b455e72ec0b26a34dc9b65cb3750baf5d8a6d19896c3b4a31b578b15ab7086377955509fad'
 
-$pgpPublicKey = "$toolsDir\MullvadVPN-2019.4.exe.asc"
-$pgpPublicKeyHash = '7f35555161c35fb83c82a7ec6d468fb7a5b1a61a18027ff14333782fe53b1285'
+$pgpPublicKey = "$toolsDir\MullvadVPN-2019.5.exe.asc"
+$pgpPublicKeyHash = '8c073c0371f93315de3fea40437e928543a0e71edb4796fde8017968256680a6'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2019.4.exe'
+  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2019.5.exe'
 
-  checksum64    = 'ce6992ec4acb3cb1e053d19390348a9ee301bf53b162f1c314a3ee96d01ffad3'
+  checksum64    = '5ada9945d59b0d2e29242270403491e7657704a07a67eb45419ce9dc7a2a56cc'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'
